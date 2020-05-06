6070030f6ad5d224c0c0bbd4435dbed067099f60
Barry Haygood <semieng94@gmail.com>
2020-05-06 17:44:24 -0500

    better fit by increasing depth

--- train.py
+++ train.py
@@ -11,6 +11,6 @@ df = pd.read_csv(path)
 # create model - we will use a gradient boosting regressor
 X, y = prep_data(df)
 
-est = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=6, random_state=0, loss='ls').fit(X, y)
+est = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=7, random_state=0, loss='ls').fit(X, y)
 
 dump(est, "est.joblib")
